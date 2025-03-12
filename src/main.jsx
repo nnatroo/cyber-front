06befa3a2ca4687e1529be7ca99db6c0d346f4b6
nnatroo <nugonatroshvili@gmail.com>
2025-03-12 20:32:30 +0400

    Add Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import './reset.css'
-import App from './App.jsx'
+import './reset.scss'
+import {BrowserRouter, Route, Routes} from "react-router";
+import {Contact} from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
 )
