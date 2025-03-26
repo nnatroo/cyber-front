39ae7b39225be097d93858fe7d154489aaf41a46
Your Name <niko.ramishvili@gmail.com>
2025-03-26 20:17:19 +0400

    4: Add categories section

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import './reset.scss'
 import {BrowserRouter, Route, Routes} from "react-router";
 import {Contact} from "./pages/Contact.jsx";
 import Home from "./pages/Home.jsx";
+import {Categories} from "./components/Categories.jsx";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Categories />} />
+
     </Routes>
   </BrowserRouter>
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import {Header} from "../components/Header.jsx";
+import {Categories} from "../components/Categories.jsx";
 
 const Home = () => {
   return (
     <>
      <Header />
+        <Categories/>
     </>
   )
 };
