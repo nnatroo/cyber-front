d02d82af874512e50be356a224efb333bf40b9be
muzakl <themuzawz@gmail.com>
2025-04-23 14:55:55 +0400

    8: lodash chunker and working pagination

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,37 +5,45 @@ import arrowDown from '../assets/arrow-down.svg'
 import like from '../assets/like.svg'
 import arrowLeft from '../assets/left-arrow.svg'
 import arrowRight from '../assets/right-arrow.svg'
-import {Link} from 'react-router'
-import {useState, useEffect} from "react";
+import { Link } from 'react-router'
+import { useState, useEffect } from "react"
 import axios from 'axios'
+import chunk from 'lodash.chunk'
 
 const Product = () => {
     const [smartphones, setSmartphones] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
 
     useEffect(() => {
         axios.get('http://localhost:5000/products/smartphones')
             .then(function (response) {
-                setSmartphones(response.data);
-                console.log(response.data);
+                setSmartphones(response.data)
+                console.log(response.data)
             })
             .catch(function (error) {
-                console.log(error);
+                console.log(error)
             })
-    }, []);
+    }, [])
 
     const likeButtonHandler = (item) => {
-        const likedItems = JSON.parse(localStorage.getItem('likedItems')) || [];
-        likedItems.push(item);
-        localStorage.setItem('likedItems', JSON.stringify(likedItems));
+        const likedItems = JSON.parse(localStorage.getItem('likedItems')) || []
+        likedItems.push(item)
+        localStorage.setItem('likedItems', JSON.stringify(likedItems))
     }
+
+    const pageHandler = (number) => {
+        setCurrentPage(number)
+    }
+
+    const chunkedSmartphones = chunk(smartphones, Math.ceil(smartphones.length / 3))
 
     return (
         <div className={classes['main-div']}>
             <nav className={classes['breadcrumb']}>
                 <Link to={"/"}>Home</Link>
-                <img src={arrow} alt={"Arrow icon"}/>
+                <img src={arrow} alt={"Arrow icon"} />
                 <Link to={"/"}>Catalog</Link>
-                <img src={arrow} alt={"Arrow icon"}/>
+                <img src={arrow} alt={"Arrow icon"} />
                 <Link to={"/product"} className={classes['active']}>Smartphones</Link>
             </nav>
             <div className={classes['main-container']}>
@@ -44,27 +52,27 @@ const Product = () => {
                         <h1>Brand</h1>
                     </figure>
                     <figure className={classes['option-holder']}>
-                        <img src={checkboxIcon} alt={"Checkbox icon"}/>
+                        <img src={checkboxIcon} alt={"Checkbox icon"} />
                         <h1>Apple</h1>
                     </figure>
                 </div>
                 <div className={classes['product-wrapper']}>
                     <figure className={classes['top-part']}>
                         <div className={classes['header-container']}>
-                            <h1>Selected Products: <span>85</span></h1>
+                            <h1>Selected Products: <span>{smartphones.length}</span></h1>
                         </div>
                         <div className={classes['dropdown-container']}>
                             <h1>By rating</h1>
-                            <img src={arrowDown} alt={"Arrow down icon"}/>
+                            <img src={arrowDown} alt={"Arrow down icon"} />
                         </div>
                     </figure>
                     <div className={classes['item-container']}>
-                        {smartphones.map((item, index) => (
+                        {chunkedSmartphones[currentPage - 1]?.map((item, index) => (
                             <div key={index} className={classes['item']}>
-                                <button onClick={() => likeButtonHandler(item)} className={classes['like-button']}><img className={classes['like-img']}
-                                                                                src={like}/>
+                                <button onClick={() => likeButtonHandler(item)} className={classes['like-button']}>
+                                    <img className={classes['like-img']} src={like} alt="Like icon" />
                                 </button>
-                                <img src={`http://localhost:5000/${item.picture}`} alt={"Phone image"}/>
+                                <img src={`http://localhost:5000/${item.picture}`} alt={"Phone image"} />
                                 <h1>{item.name}</h1>
                                 <p>{item.finalPrice}</p>
                                 <button className={classes['buy-button']}><span>Buy now</span></button>
@@ -74,11 +82,11 @@ const Product = () => {
                 </div>
             </div>
             <div className={classes['pagination-container']}>
-                <button className={classes['arrow-button']}><img src={arrowLeft}/></button>
-                <button className={classes['active-button']}>1</button>
-                <button className={classes['page-button']}>2</button>
-                <button className={classes['page-button']}>3</button>
-                <button className={classes['arrow-button']}><img src={arrowRight}/></button>
+                <button className={classes['arrow-button']}><img src={arrowLeft} alt="Previous" /></button>
+                <button onClick={() => pageHandler(1)} className={currentPage === 1 ? classes['active-button'] : classes['page-button']}>1</button>
+                <button onClick={() => pageHandler(2)} className={currentPage === 2 ? classes['active-button'] : classes['page-button']}>2</button>
+                <button onClick={() => pageHandler(3)} className={currentPage === 3 ? classes['active-button'] : classes['page-button']}>3</button>
+                <button className={classes['arrow-button']}><img src={arrowRight} alt="Next" /></button>
             </div>
         </div>
     )
