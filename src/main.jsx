7d7eade6e6b013e3f8de0eb2d02a80caadc01317
vengence1 <d.khabuliani@outlook.com>
2025-04-30 19:21:24 +0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './reset.scss'
 import {BrowserRouter, Route, Routes} from "react-router";
-import {Contact} from "./pages/Contact.jsx";
-import {Address} from "./pages/Address.jsx";
+import {Payment} from "./pages/Payment.jsx";
 import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/address" element={<Address />} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/payment" element={<Payment />} />
+        </Routes>
+    </BrowserRouter>
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import {Header} from "../components/Header.jsx";
-import {Categories} from "../components/Categories.jsx";
-import SaleBanner from "../components/SaleBanner.jsx";
-import HeroSection from "../components/HeroSection.jsx";
+
 
 const Home = () => {
     return (
         <>
             <Header/>
-            <HeroSection/>
-            <Categories/>
-            <SaleBanner />
         </>
     )
 };
