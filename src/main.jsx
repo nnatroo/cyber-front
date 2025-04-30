b893b8c97e54a7054ea8955511cc32dbaadf0419
nikanika173 <147316302+nikanika173@users.noreply.github.com>
2025-04-30 16:31:54 +0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './reset.scss'
-import {BrowserRouter, Route, Routes} from "react-router";
-import {Contact} from "./pages/Contact.jsx";
-import {Address} from "./pages/Address.jsx";
-import Home from "./pages/Home.jsx";
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/address" element={<Address />} />
-    </Routes>
-  </BrowserRouter>
+    <App />
 )
