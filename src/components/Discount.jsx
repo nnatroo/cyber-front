a1a5037606226269af8fd8fe6ae533944a6e6595
ssbombaguy <giorgimaisuradze2008@gmail.com>
2025-05-02 14:52:38 +0400

    rendering hearts and functionality

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -1,9 +1,15 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import classes from "../modules/Discounts.module.css";
+import redHeart from "../assets/redHeart.svg";
+import heart from "../assets/heart.svg";
 export const Discount = () => {
     const [productData, setProductData] = useState([])
-
+    const [favourite, setFavourite] = useState(false);
+    const [favouriteData, setFavouriteData] = useState([]);
+    const favouriteHandler = () => {
+        setFavourite(!favourite)
+    }
     useEffect(() => {
         axios.get(`http://localhost:5000/products/discounts`)
             .then(function (response) {
@@ -24,11 +30,16 @@ export const Discount = () => {
                 <div className={classes["discounts-product"]}>
                     {productData.map((product, index) => (
                         <div key={index} className={classes["discounts-item"]}>
+                            <div className={classes["discounts-heart"]}>
+                                <button className={classes["heart-btn"]} onClick={()=>{favouriteHandler}}>{favourite ?
+                                    <img src={redHeart} alt="redheart"/>:<img src={heart} alt="heart"/>
+                                }</button>
+                            </div>
                             <figure className={classes["discounts-image"]}>
                                 <img src={`http://localhost:5000/${product.image}`} alt={product.category}/>
                             </figure>
                             <div className={classes["discounts-info"]}>
-                            <p>{product.name}</p>
+                                <p>{product.name}</p>
                                 <h3>${product.price}</h3>
                             </div>
 
--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import classes from "../modules/NewArrivals.module.css";
+import redHeart from "../assets/redHeart.svg";
+import heart from "../assets/heart.svg";
 
 export const NewArrivals = () => {
     const [productData, setProductData] = useState([]);
-
+    const [favourite, setFavourite] = useState(false);
+    const [favouriteData, setFavouriteData] = useState([]);
+    const favouriteHandler = () => {
+        setFavourite(!favourite)
+    }
     useEffect(() => {
         axios.get(`http://localhost:5000/products/newArrival`)
             .then(function (response) {
@@ -22,8 +28,14 @@ export const NewArrivals = () => {
                 <div className={classes["product-card"]}>
                     {productData.map((product, index) => (
                         <div key={index} className={classes["new-arrival-item"]}>
+                            <div className={classes["arrivals-heart"]}>
+                                <button className={classes["heart-btn"]} onClick={() => {
+                                    favouriteHandler
+                                }}>{favourite ?
+                                    <img src={redHeart} alt="redheart"/> : <img src={heart} alt="heart"/>}</button>
+                            </div>
                             <figure className={classes["new-arrival-image"]}>
-                                <img src={`http://localhost:5000/${product.image}`} alt={product.category} />
+                                <img src={`http://localhost:5000/${product.image}`} alt={product.category}/>
                             </figure>
                             <div className={classes["new-arrival-info"]}>
                                 <p>{product.name}</p>
@@ -33,9 +45,9 @@ export const NewArrivals = () => {
                             <button className={classes["buy-btn"]}>Buy Now</button>
                         </div>
 
-                        ))}
-                     </div>
+                    ))}
                 </div>
-            </>
-            );
-            };
+            </div>
+        </>
+    );
+};
