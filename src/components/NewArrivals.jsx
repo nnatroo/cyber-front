5ab8061770f8aa1166191f10353b57922d1b5198
daviti558 <davit.tchelidzee@gmail.com>
2025-05-02 19:13:32 +0400

    update 01/05

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,16 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import classes from "../modules/NewArrivals.module.css";
-import redHeart from "../assets/redHeart.svg";
+import classes from "../modules/NewArrivals.module.scss";
 import heart from "../assets/heart.svg";
 
 export const NewArrivals = () => {
     const [productData, setProductData] = useState([]);
-    const [favourite, setFavourite] = useState(false);
-    const [favouriteData, setFavouriteData] = useState([]);
-    const favouriteHandler = () => {
-        setFavourite(!favourite)
-    }
+
     useEffect(() => {
         axios.get(`http://localhost:5000/products/newArrival`)
             .then(function (response) {
@@ -19,32 +14,33 @@ export const NewArrivals = () => {
             })
             .catch(function (error) {
                 console.log(error);
-            })
+            });
     }, []);
 
     return (
         <>
+            <div className={classes["category-section"]}>
+                <h2 className={classes["new-arrival-heading"]}>New Arrival</h2>
+                <h2>Bestseller</h2>
+                <h2>Featured products</h2>
+            </div>
             <div className={classes["product-container"]}>
                 <div className={classes["product-card"]}>
-                    {productData.map((product, index) => (
-                        <div key={index} className={classes["new-arrival-item"]}>
+                    {productData.map((product) => (
+                        <div key={product._id || product.id} className={classes["new-arrival-item"]}>
                             <div className={classes["arrivals-heart"]}>
-                                <button className={classes["heart-btn"]} onClick={() => {
-                                    favouriteHandler
-                                }}>{favourite ?
-                                    <img src={redHeart} alt="redheart"/> : <img src={heart} alt="heart"/>}</button>
+                                <img src={heart} alt="heart icon" />
                             </div>
                             <figure className={classes["new-arrival-image"]}>
-                                <img src={`http://localhost:5000/${product.image}`} alt={product.category}/>
+                                <img src={`http://localhost:5000/${product.image}`}
+                                     alt={product.name || product.category}/>
                             </figure>
                             <div className={classes["new-arrival-info"]}>
                                 <p>{product.name}</p>
                                 <h3>${product.price}</h3>
                             </div>
-
                             <button className={classes["buy-btn"]}>Buy Now</button>
                         </div>
-
                     ))}
                 </div>
             </div>
