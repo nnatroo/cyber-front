75eb7d9015b324d30d640cb9788c0ff5668c20b6
nnatroo <nugonatroshvili@gmail.com>
2025-05-12 19:37:28 +0400

    8: Revert Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import {Header} from "../components/Header.jsx";
+import {Footer} from "../components/Footer.jsx";
 import {Categories} from "../components/Categories.jsx";
 import SaleBanner from "../components/SaleBanner.jsx";
 import HeroSection from "../components/HeroSection.jsx";
+import {NewArrivalSection} from "../components/NewArrivalSection.jsx"
+import {DiscountSection} from "../components/DiscountSection.jsx"
 
 const Home = () => {
     return (
@@ -9,7 +12,10 @@ const Home = () => {
             <Header/>
             <HeroSection/>
             <Categories/>
+            <NewArrivalSection/>
+            <DiscountSection/>
             <SaleBanner />
+            <Footer/>
         </>
     )
 };
