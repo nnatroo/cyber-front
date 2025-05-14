19e9ee716682fb7a4cc528bdc435e902c0377bb4
nnatroo <nugonatroshvili@gmail.com>
2025-05-14 19:17:15 +0400

    22: Merge master

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import {createRoot} from 'react-dom/client'
 import './reset.scss'
 import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./pages/Home.jsx";
 import {Contact} from "./pages/Contact.jsx";
 import {Address} from "./pages/Address.jsx";
-import Home from "./pages/Home.jsx";
-import Error from './pages/Error.jsx'
+import {Error} from './pages/Error.jsx'
 import {ShoppingCart} from "./pages/Cart.jsx";
 import {Provider} from 'react-redux';
 import {store} from './redux/store.js';
+import {Shipping} from "./pages/Shipping.jsx";
 
 
 createRoot(document.getElementById('root')).render(
