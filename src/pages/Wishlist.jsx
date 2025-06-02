262a566f2e3a7bb541f60a1e89695a3b3a7048e9
macintoshi <dekodumbadze@gmail.com>
2025-06-02 19:41:53 +0400

    #39 quizn1

---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,16 @@
+import classes from '../modules/Wishlist.module.scss'
+import Header from "../components/Header.jsx";
+import {Footer} from "../components/Footer.jsx";
+
+export const Wishlist = () => {
+    return (
+        <>
+            <Header/>
+         <div className={classes['main-wrapper']}>
+
+
+         </div>
+            <Footer></Footer>
+        </>
+    )
+}
