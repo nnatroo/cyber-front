3e66fc06305b91e4cdc280e7fe43c324081909cf
nnatroo <nugonatroshvili@gmail.com>
2025-06-11 19:55:45 +0400

    Fix issues

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,16 +6,22 @@ import {useNavigate} from "react-router";
 export const Categories = () => {
     const [categories, setCategories] = useState([]);
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(() => {
+        setIsLoading(true);
         axios.get('http://localhost:5000/categories')
             .then((response) => {
-                setCategories(response.data);
+                setCategories(response.data.slice(0, 6));
             })
             .catch((error) => {
                 console.error(error);
-
+            })
+            .finally(() => {
+                setIsLoading(false);
             });
     }, []);
+
     const handleCategoryClick = (img) => {
         navigate(`/products/${img.categoryName}`);
         window.scrollTo(0, 0);
@@ -28,9 +34,10 @@ export const Categories = () => {
                         <h3>Browse By Category</h3>
                     </div>
                     <div className={classes["category-cards-wrapper"]}>
-                        {categories.map((img, index) => (
+                        {isLoading && <div className={classes["loading-wrapper"]}>...Loading</div>}
+                        {categories.map((category, index) => (
                             <figure key={index} className={classes["category-card"]}  onClick={() => handleCategoryClick(img)}>
-                                <p className={classes["category-card-text"]}>{img.categoryName}</p>
+                                <p className={classes["category-card-text"]}>{category.name}</p>
                             </figure>
                         ))}
                     </div>
